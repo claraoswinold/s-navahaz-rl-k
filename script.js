72eed758f6d16b6e405301a709deaa82b2737d9d
claraoswinold <159765007+claraoswinold@users.noreply.github.com>
2025-03-31 12:35:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.querySelectorAll("button").forEach(function(button) {
-    button.addEventListener("click", function() {
-        alert("Ödeme Yapılmadı, Ödeme Sayfasına Yönlendirileceksiniz.");
-    });
-});
+
